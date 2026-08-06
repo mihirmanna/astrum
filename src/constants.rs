25642525fe68e8e0401e9ac50f92d2c3bb19a6ca
Mihir Manna <mihirmanna2@gmail.com>
2026-08-06 01:07:19 -0500

    Add more physical constants

--- src/constants.rs
+++ src/constants.rs
@@ -11,3 +11,18 @@ pub(crate) const C: f64 = 2.99792458e10;
 
 /// Newton's gravitational constant measured in dyn.cm^2/g^2
 pub(crate) const G: f64 = 6.6743e-8;
+
+/// Boltzmann constant measured in erg/K
+pub(crate) const K_B: f64 = 1.380649e-16;
+
+/// Planck constant measured in erg/Hz
+pub(crate) const H_PLANCK: f64 = 6.62607015e-27;
+
+/// Electron mass measured in g
+pub(crate) const M_E: f64 = 9.1093837139e-28;
+
+/// Avogadro number measured in 1/mol
+pub(crate) const N_A: f64 = 6.02214076e23;
+
+/// Hydrogen ionization energy measured in erg
+pub(crate) const PHI_H: f64 = 2.179872361103e-11;
